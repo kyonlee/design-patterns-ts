00b810abf3af1bdb664f399e828a07692d785de8
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-29 20:15:02 +0900

    Add fetchTodos action creator

---
+++ react-redux-ts/src/actions/index.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+
+import { ActionTypes } from './types';
+
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+interface FetchTodosAction {
+	type: ActionTypes.FetchTodos;
+	payload: Todo[];
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+	return async (dispatch: Dispatch) => {
+		const response = await axios.get<Todo[]>(url);
+
+		dispatch<FetchTodosAction>({
+			type: ActionTypes.FetchTodos,
+			payload: response.data
+		});
+	};
+};
